567ac0e102c514574e33f6b08644297cdcbc5c86
dominik <dominiksk47@gmail.com>
2026-04-23 08:33:17 +0200

    face and eye detection

--- src/CornerDetection.py
+++ src/CornerDetection.py
@@ -14,12 +14,12 @@ for corner in corners:
     cv2.circle(img, (x,y), 5, (0,255,0), -1)
 
 # line for each corners
-for i in range(len(corners)):
-    for j in range(i+1, len(corners)):
-        corner1 = tuple(corners[i][0])
-        corner2 = tuple(corners[j][0])
-        color = tuple(map(lambda x: int(x), np.random.randint(0, 255, size=3)))
-        cv2.line(img, corner1, corner2, color, 1)
+# for i in range(len(corners)):
+#     for j in range(i+1, len(corners)):
+#         corner1 = tuple(corners[i][0])
+#         corner2 = tuple(corners[j][0])
+#         color = tuple(map(lambda x: int(x), np.random.randint(0, 255, size=3)))
+#         cv2.line(img, corner1, corner2, color, 1)
 
 cv2.imshow('img', img)
 cv2.waitKey(0)
